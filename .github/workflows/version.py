4e8af8304194ee20cdab023b5f1e2d00af1002c0
Justine Belza <justine.belza@innovahomes.ph>
2025-03-31 10:01:06 +0800

    test

--- .github/workflows/version.py
+++ .github/workflows/version.py
@@ -1,5 +1,13 @@
 import os
 
-w = open("version.txt", "w")
-w.write("Test")
-w.close()
+with open('version.txt', 'w+') as file:
+    try:  # catch OSError in case of a one line file 
+        file.seek(-2, os.SEEK_END)
+        while file.read(1) != b'\n':
+            file.seek(-2, os.SEEK_CUR)
+    except OSError:
+        file.seek(0)
+    last_line = file.readline().decode()
+
+    file.write(last_line)
+
