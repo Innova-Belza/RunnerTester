a525428e2c10f40dfdfa9e32191716b98586ce7d
Justine Belza <justine.belza@innovahomes.ph>
2025-03-31 10:18:18 +0800

    asdf

--- .github/workflows/version.py
+++ .github/workflows/version.py
@@ -1,5 +1,15 @@
 import os
 
+type = ""
+with open('version.txt', 'r') as file:
+    try:  # catch OSError in case of a one line file 
+        file.seek(-2, os.SEEK_END)
+        while file.read(1) != b'\n':
+            file.seek(-2, os.SEEK_CUR)
+    except OSError:
+        file.seek(0)
+    type = file.readline()
+
 with open('version.txt', 'a+') as file:
     try:  # catch OSError in case of a one line file 
         file.seek(-2, os.SEEK_END)
@@ -9,10 +19,16 @@ with open('version.txt', 'a+') as file:
         file.seek(0)
     last_line = file.readline()
 
-    last_line.replace("version=", "")
+    last_line = last_line.replace("version=", "")
     last_line = last_line.split(".")
 
-    last_line = last_line[0] + "." + last_line[1] + "." + str(int(last_line[2]) + 1)
+    type = type.lower();  
+    if type == "version":
+        last_line = str(int(last_line[0]) + 1) + ".0.0"
+    elif type == "major":
+        last_line = last_line[0] + "." + str(int(last_line[1]) + 1) + ".0"
+    else:
+        last_line = last_line[0] + "." + last_line[1] + "." + str(int(last_line[2]) + 1)
 
     file.write("\nversion=" + last_line)
 
