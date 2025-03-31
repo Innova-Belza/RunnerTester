6805ce679ff4ad14a36db7c34619144b41c44880
Justine Belza <justine.belza@innovahomes.ph>
2025-03-31 10:05:59 +0800

    test

--- .github/workflows/version.py
+++ .github/workflows/version.py
@@ -1,6 +1,6 @@
 import os
 
-with open('version.txt', 'w+') as file:
+with open('version.txt', 'a+') as file:
     try:  # catch OSError in case of a one line file 
         file.seek(-2, os.SEEK_END)
         while file.read(1) != b'\n':
